27a0ad54a6c440ba099c36dd66e1413b50c5c532
csapazah <csapazah@unjbg.edu.pe>
2026-06-19 20:48:33 -0500

    Actualizacion del codigo y configuro ignore

--- A_reverso.cpp
+++ A_reverso.cpp
@@ -6,8 +6,8 @@ int main(){
     for(int i = 0 ; i<=n-1 ; i++){
         cin >> A[i];
     }
-    for(int i = n-1; i>=0;i++){
-        cout << A[i];
+    for(int i = n-1; i>=0;i--){
+        cout << A[i] << " ";
     }
 
     return 0;
