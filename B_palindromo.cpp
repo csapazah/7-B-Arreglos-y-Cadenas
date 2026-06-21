da5733169db1a0a6177b5923dcef44bbd32d9a0b
csapazah <csapazah@unjbg.edu.pe>
2026-06-20 20:58:37 -0500

    Lectura de cada palabra y su largo

--- B_palindromo.cpp
+++ B_palindromo.cpp
@@ -3,5 +3,14 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+    for (int i = 0; i < n; i++){
+        char palabra[50];
+        cin >> palabra;
+
+        int p_largo = 0;
+        while (palabra[p_largo] != '\0'){
+            p_largo++;
+        }
+    }
     return 0;
 }
