2ef2a0531a45eeb34a2146d864743b0609f98d71
csapazah <csapazah@unjbg.edu.pe>
2026-06-21 17:09:08 -0500

    Asignacion de voto de cada alumno

--- C_frecuencias.cpp
+++ C_frecuencias.cpp
@@ -4,6 +4,11 @@ int main(){
     int frec[101] = {0};
     int p,n;
     cin >> p >> n;
+    for(int i = 0; i < n; i++){
+        int f;
+        cin >> f;
+        frec[f]++;
+    }
 
     return 0;
 }
