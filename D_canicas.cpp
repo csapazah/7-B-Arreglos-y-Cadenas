832431d926638e889ee993bd6082d4d20a6954bd
csapazah <csapazah@unjbg.edu.pe>
2026-06-22 19:41:54 -0500

    Conteo de canicas de cada vaso

--- D_canicas.cpp
+++ D_canicas.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main(){
     int n,c;
     cin >> n >> c;
+    for(int i = 0;i < c; i++){
+        int conteo;
+        cin >> conteo;
+        Vasos[conteo]++;
+    }
     return 0;
 }
